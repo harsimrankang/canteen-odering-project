3deb8ea830d58d189319a2e9fa39b46c99f7abbb
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-02-11 10:03:37 +0530

    fixed menu

--- src/components/addButtonFunc.js
+++ src/components/addButtonFunc.js
@@ -1,20 +1,16 @@
-
 var count;
 if (!count) {
-    count = 0;
+  count = 0;
 }
 
-
-function incrementCount() {
-    count++;
-    document.getElementsByClassName('count').innerHTML = count;
+function incrementCount(key) {
+  count++;
+  document.getElementById(key).value = count;
 }
 
-function decrementCount() {
-    count--;
-    document.getElementsByClassName('count').innerHTML = count;
+function decrementCount(key) {
+  count--;
+  document.getElementById(key).value = count;
 }
 
-
-
-
+export { incrementCount, decrementCount };
