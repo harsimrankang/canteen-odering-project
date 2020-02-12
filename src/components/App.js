1b851ef50d76f392fcb0489c024bf6565426efb9
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-02-12 09:50:23 +0530

    changes to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
-import {
-  HashRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -13,13 +9,17 @@ import Burgrill from "./Burgrill";
 class App extends Component {
   render() {
     return (
-      <Router basename='/'>
+      <Router basename="/">
         <div>
-          <Switch>
-            <Route exactpath="/Burgrill"><Burgrill /></Route>
-          </Switch>
           <Navbar />
-          <Orders />
+          <Switch>
+            <Route path="/Burgrill">
+              <Burgrill />
+            </Route>
+            <Route path="/Orders">
+              <Orders />
+            </Route>
+          </Switch>
         </div>
       </Router>
     );
