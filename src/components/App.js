84f594e09e0243668713f974bd2769717876256f
harmanpreetkaur689 <harmanpreetkaur689@gmail.com>
2020-02-19 23:09:21 +0530

    on login click

--- src/components/App.js
+++ src/components/App.js
@@ -27,6 +27,7 @@ class App extends Component {
   componentDidMount() {
     this.props.firebase.auth.onAuthStateChanged(
       authUser => {
+        if (this.state.user == null && authUser != null) this.props.history.push('/mainpage');
         if (authUser != this.state.user)
           authUser
             ? this.setState({ user: authUser })
