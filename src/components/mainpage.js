1ca6fd43008e59fc1fa20cc7b564ff376b28f76a
harmanpreetkaur689 <harmanpreetkaur689@gmail.com>
2020-02-24 14:14:20 +0530

    hover changes

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -20,7 +20,7 @@ class Mainpage extends Component {
               style={{ overflow: "hidden" }}
             >
               <div class="card-body">
-                <div><h5 class="card-title">LA PINOZ</h5></div>
+                <div> <h5 class="card-title d-inline-flex" >LA PINOZ</h5></div>
                 <p class="card-text">All the taste and toppings you want on a pizza, straight from our oven to your door!</p>
                 <a href="#" class="btn btn-primary">EXPLORE</a>
               </div>
@@ -36,7 +36,7 @@ class Mainpage extends Component {
               style={{ overflow: "hidden" }}
             >
               <div class="card-body">
-                <div><h5 class="card-title">BURGRILL</h5></div>
+                <div> <h5 class="card-title d-inline-flex" >BURGRILL</h5></div>
                 <p class="card-text">Liscensed to grill.</p>
                 <a href="#" class="btn btn-primary">EXPLORE</a>
               </div>
@@ -48,8 +48,8 @@ class Mainpage extends Component {
               className="menutitlecard card bg-secondary m-1 embed-responsive embed-responsive-16by9"
               style={{ overflow: "hidden" }}
             >
-              <div class="card-body">v
-                <div><h5 class="card-title">TEA Q</h5></div>
+              <div class="card-body">
+                <div> <h5 class="card-title d-inline-flex" >TEA Q</h5></div>
                 <p class="card-text">Adding Moments of Perfect Sip.</p>
                 <a href="#" class="btn btn-primary">EXPLORE</a>
               </div>
@@ -62,9 +62,9 @@ class Mainpage extends Component {
               style={{ overflow: "hidden" }}
             >
               <div class="card-body">
-                <div>
-                  <h5 class="card-title d-inline-flex" >SQUARE ONE</h5>
-                </div>
+
+                <h5 class="card-title d-inline-flex" >SQUARE ONE</h5>
+
 
                 <p class="card-text"> It's good mood food.</p>
                 <a href="#" class="btn btn-primary">EXPLORE</a>
