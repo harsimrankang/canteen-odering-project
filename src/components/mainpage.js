ad66d833eb815d56fa6f6adfaaac2745e90c6b05
harmanpreetkaur689 <harmanpreetkaur689@gmail.com>
2020-02-24 15:54:36 +0530

    card as a link orders

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import "./mainpage.css";
+import { Link } from "react-router-dom";
 
 class CardItems extends Component {
   render() {
     return (
+     
       <div className="col-xs-12 col-md-6 col-xl-6">
+        
         <div
           className="card bg-secondary m-1 my-2 embed-responsive embed-responsive-16by9"
           style={{ overflow: "hidden" }}
         >
+          <Link to={this.props.page} style={{textDecoration:"none",color: "black"}}>
           <div class="card-body">
             <div>
               <h5 class="card-title d-inline-flex">{this.props.title}</h5>
@@ -16,12 +20,13 @@ class CardItems extends Component {
             <p class="card-text" style={{ height: "48px" }}>
               {this.props.para}
             </p>
-            <a href="#" class="btn btn-primary">
-              EXPLORE
-            </a>
+           
           </div>
+          </Link>
         </div>
+        
       </div>
+      
     );
   }
 }
@@ -48,14 +53,18 @@ class Mainpage extends Component {
 
         <div className="container-fluid">
           <div className="row">
+                
             <CardItems
               title="LA PINOZ"
               para="All the taste and toppings you want on a pizza, straight from
-                  our oven to your door!"
+                  our oven to your door!" page="orders"
             />
-            <CardItems title="BURGRILL" para="Liscensed to grill." />
+           
+            <CardItems title="BURGRILL" para="Liscensed to grill."  />
+           
             <CardItems title="TEA Q" para="Adding Moments of Perfect Sip." />
             <CardItems title="SQUARE ONE" para="It's good mood food." />
+           
           </div>
         </div>
       </div>
