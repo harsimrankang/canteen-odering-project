774dc370b8b09f60f433f547a21fa61c5f602f3b
harmanpreetkaur689 <harmanpreetkaur689@gmail.com>
2020-04-09 00:16:23 +0530

    navigation changes

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,19 +4,23 @@ import SignUp from '../SignUp';
 import NavAuth from './NavAuth';
 import { Link } from 'react-router-dom';
 
-const Navigation = ({ user, username }) => (
+/*const Navigation = ({ user, username }) => (
     <div className="row justify-content-center justify-content-sm-end">
         {user ? <NavAuth authUser={user} username={username} /> :
             <NonAuth />}
     </div>
-)
-class NonAuth extends Component {
+)*/
+class Navigation extends Component {
     render() {
         return (
-            <div>
-                <Link className="btn btn-dark" to="Login">LOGIN</Link>
-                <Link className="btn btn-dark" to="SignUp">SIGNUP</Link>
+            <div className="row justify-content-center justify-content-sm-end">
+                {this.user ? <NavAuth authUser={this.props.user} username={this.props.username} /> :
+                    <div>
+                        <Link className="btn btn-dark" to="Login">LOGIN</Link>
+                        <Link className="btn btn-dark" to="SignUp">SIGNUP</Link>
+                    </div>}
             </div>
+
         );
     }
 }
