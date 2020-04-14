977a5d77db2856d8fb03885210c9c51846ec8a2b
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-04-14 23:17:18 +0530

    updates

--- src/components/ultraMenuPage.js
+++ src/components/ultraMenuPage.js
@@ -1,11 +1,74 @@
 import React, { Component } from "react";
+import { withFirebase } from "./Firebase";
 class UltraMenuPage extends Component {
+  state = {
+    selectedVendors: [],
+    unselectedVendors: [],
+    selectedCategories: [],
+    unselectedCategories: [],
+  };
+  componentDidMount() {
+    this.getData();
+  }
+  getData = () => {
+    this.props.firebase.db.ref("public").on("value", (snapshot) => {
+      console.log("updated");
+      var res = snapshot.val();
+      //console.log(res);
+
+      var allCategories = [];
+      Object.keys(res["menuCategories"]).forEach((key) => {
+        //allCategories.push({ key: res["menuCategories"][key] });
+        //allCategories[key] = res["menuCategories"][key];
+        allCategories.push({ id: key, value: res["menuCategories"][key] });
+      });
+
+      var vendors = [];
+      Object.keys(res["vendors"]).forEach((key) => {
+        //vendors.push(value);
+        //vendors[key] = res["vendors"][key];
+        vendors.push({ id: key, value: res["vendors"][key] });
+      });
+
+      var allSizes = [];
+      Object.keys(res["sizeCategories"]).forEach((key) => {
+        allSizes.push({ id: key, value: res["sizeCategories"][key] });
+      });
+
+      var items = [];
+      Object.keys(res["items"]).forEach((key) => {
+        //allCategories.push({ key: res["menuCategories"][key] });
+        //allCategories[key] = res["menuCategories"][key];
+        items.push({ id: key, value: res["items"][key] });
+      });
+      console.log(vendors);
+
+      this.setState({
+        fetchedData: res,
+        items: items,
+        searchedCategories: allCategories,
+        allCategories: allCategories,
+        vendors: vendors,
+        unselectedSizes: allSizes,
+        allSizes: allSizes,
+        selectedSizes: [],
+      });
+    });
+  };
+  showUnselectedVendors = () => {
+    return (
+      <div className="col-12 d-flex">
+        <div className="flex-fill"></div>
+      </div>
+    );
+  };
   render() {
     return (
       <div className="col-12">
+        ULTRAMENU
         <div className="col-12" id=""></div>
       </div>
     );
   }
 }
-export default UltraMenuPage;
+export default withFirebase(UltraMenuPage);
